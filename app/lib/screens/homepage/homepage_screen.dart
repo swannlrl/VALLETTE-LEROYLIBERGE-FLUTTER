2d6af331dc33741e179b7f7548eb6f699b7e20a9
Swann Le Roy Liberge <swann02.lrl@gmail.com>
2026-03-24 22:14:32 +0100

    correction mineure des pages favoris et accueil

--- app/lib/screens/homepage/homepage_screen.dart
+++ app/lib/screens/homepage/homepage_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:formation_flutter/api/open_food_facts_api.dart';
 import 'package:formation_flutter/api/pocketbase_api.dart';
